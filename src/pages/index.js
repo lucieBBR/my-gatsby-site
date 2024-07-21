e30c4ed3f1ff52973a3594573871bd4308ef58b8
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-21 19:37:32 +0200

    component import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,16 @@
 
 import * as React from 'react'
 import Layout from '../components/layout'
+import MyIntro from '../components/myIntro';
 import "../styles/global.css";
-import {
-  introText,
-  greeting,
-  aboutText
-} from "../styles/global.css";
 import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="">
-      <div className={introText}>
-        <h1 className={greeting}>Hi. I'm Lucie. <br/> A fullstack developer.</h1>
-        <p className={aboutText}>I'm passionate about sustainability and projects with a positive social impact.</p>
-        {/* <StaticImage
-          alt="Piece of code"
-          src="../images/florian-olivo-4hbJ-eymZ1o-unsplash.jpg"
-        /> */}
-      </div>
+      <main>
+        <MyIntro/>  
+      </main>   
     </Layout>
   )
 }
